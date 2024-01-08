3985afc2d0a1fc1eb452d340126c26e55cd04491
Serhii Avdieiev <reflextay@gmail.com>
2024-01-08 14:36:23 +0200

    Main Application files

--- src/App.js
+++ src/App.js
@@ -1,24 +1,15 @@
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import CharactersList from './components/CharactersList.js';
+import CharacterCard from './components/CharacterCard.js';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>  
+      <Routes>
+        <Route exact path="/" component={CharactersList} />
+        <Route path="/character/:id" component={CharacterCard} />        
+      </Routes>
+    </Router>
   );
 }
 
