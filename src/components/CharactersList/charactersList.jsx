fc56e986fd3259a9263833608fa62dcd52b4387a
Serhii Avdieiev <reflextay@gmail.com>
2024-01-08 19:35:34 +0200

    Added Pagination

--- src/components/CharactersList/charactersList.jsx
+++ src/components/CharactersList/charactersList.jsx
@@ -1,12 +1,15 @@
-import React, { useState, useEffect } from 'react';
+import { useState, useEffect } from 'react';
 import axios from 'axios';
 import CharacterCard from '../CharacterCard/characterCard';
+import Typography from '@mui/material/Typography';
+import Pagination from '@mui/material/Pagination';
+import Stack from '@mui/material/Stack';
 import styled, { css } from 'styled-components';
 
 const CharactersWrapper = styled.div(
-  ({ theme }) => css`    
-    gap: ${theme.spacing._16};
-    padding: ${theme.spacing._40} ${theme.spacing._16} 0;
+  ({ theme }) => css`
+    gap: ${theme.spacing._24};
+    padding: ${theme.spacing._40} ${theme.spacing._16};
     display: flex;
     overflow: hidden;
     align-items: center;
@@ -17,24 +20,38 @@ const CharactersWrapper = styled.div(
   `,
 )
 
-const Title = styled.h1(
+const PaginationWrapper = styled.div(
   ({ theme }) => css`
-    
-  `,
+    text-align: center;
+    color: ${theme.backBlack};
+    background: ${theme.white};
+    padding: ${theme.spacing._24} 0;
+
+    > div {
+      display: inline-flex;
+    }
+  `    
 )
 
 const CharactersList = () => {
   const [characters, setCharacters] = useState([]);
+  const [page, setPage] = useState(1);
+  const [totalPages, setTotalPages] = useState(0);
 
   useEffect(() => {
-    axios.get('https://rickandmortyapi.com/api/character')
+    axios.get(`https://rickandmortyapi.com/api/character?page=${page}`)
       .then(response => {
         setCharacters(response.data.results);
+        setTotalPages(response.data.info.pages);        
       })
       .catch(error => { 
         console.error('Error fetching characters:', error);
       });
-  }, []);
+  }, [page, totalPages]);
+
+  const handleChange = (event, value) => {
+    setPage(value);
+  };  
 
   return (
     <>
@@ -44,8 +61,15 @@ const CharactersList = () => {
       <CharactersWrapper>
         {characters.map(character => (
           <CharacterCard key={character.id} character={character} />
-        ))}
+        ))}        
       </CharactersWrapper>
+
+      <PaginationWrapper>
+        <Stack spacing={2}>
+          <Typography>Page: {page}</Typography>
+          <Pagination count={10} page={page} onChange={handleChange} />
+        </Stack>
+      </PaginationWrapper>      
     </>
   );
 };
